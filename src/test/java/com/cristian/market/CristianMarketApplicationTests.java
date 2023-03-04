2a8be48ea199b3a122f188f71492b089a43d1e77
CristianRazo <crandres240992@gmail.com>
2023-03-04 01:58:57 -0600

    first commit

---
+++ src/test/java/com/cristian/market/CristianMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristianMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
